919c846c59221dfa5c0547283c0083d893ae77ea
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-04-25 21:08:32 -0500

    new

--- app.py
+++ app.py
@@ -5,29 +5,31 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import datetime as dt
 
-#set up database
+# set up database
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-#new model for database
+# new model for database
 Base = automap_base()
 
-#reflect the tables
+# reflect the tables
 Base.prepare(engine, reflect=True)
 
-#references for tables 
+# references for tables
 Measurement = Base.classes.measurement
 
 Station = Base.classes.station
 
-#create a session
+# create a session
 session = Session(engine)
 
 
-#set up flask 
+# set up flask
 app = Flask(__name__)
 
-#flask route 
+# flask route
+
+
 @app.route("/")
 def Home():
     return render_template("index.html")
@@ -35,11 +37,11 @@ def Home():
 
 def calc_temps(start_date, end_date):
     """TMIN, TAVG, and TMAX for a list of dates.
-    
+
     Args:
         start_date (string): A date string in the format %Y-%m-%d
         end_date (string): A date string in the format %Y-%m-%d
-        
+
     Returns:
         TMIN, TAVE, and TMAX
     """
@@ -56,8 +58,10 @@ def calc_temps(start_date, end_date):
         .all()
     )
 
-#Calculate the date 1 year ago from the last data point in the database
-last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+
+# Calculate the date 1 year ago from the last data point in the database
+last_date = session.query(Measurement.date).order_by(
+    Measurement.date.desc()).first()[0]
 last_date = dt.datetime.strptime(last_date, "%Y-%m-%d")
 last_year = last_date - dt.timedelta(days=365)
 
@@ -106,13 +110,14 @@ def tobs():
     )
     return jsonify(temp_results)
 
-    @app.route("/api/v1.0/<start>")
+
+@app.route("/api/v1.0/<start>")
 def start(start):
     """Returns the JSON list of the minimum, average and the maximum temperatures for a given start date (YYYY-MM-DD)"""
 
     temps = calc_temps(start, last_date)
 
-    #list to store the temperature records
+    # list to store the temperature records
     temp_list = []
     date_dict = {"Start Date": start, "End Date": last_date}
     temp_list.append(date_dict)
@@ -127,3 +132,32 @@ def start(start):
     )
 
     return jsonify(temp_list)
+
+
+@app.route("/api/v1.0")
+def start_end():
+    """Returns the JSON list of the minimum, average and the maximum temperatures for a given start date and end date(YYYY-MM-DD)"""
+    start = request.args.get("Start Date")
+    end = request.args.get("End Date")
+
+    temps = calc_temps(start, end)
+
+    # list to store the temperature records
+
+    temp_list = []
+    date_dict = {"Start Date": start, "End Date": end}
+    temp_list.append(date_dict)
+    temp_list.append(
+        {"Observation": "Minimum Temperature", "Temperature(F)": temps[0][0]}
+    )
+    temp_list.append(
+        {"Observation": "Average Temperature", "Temperature(F)": temps[0][1]}
+    )
+    temp_list.append(
+        {"Observation": "Maximum Temperature", "Temperature(F)": temps[0][2]}
+    )
+    return jsonify(temp_list)
+
+    # run app
+    if __name__ == "__main__":
+    app.run(debug=True)
