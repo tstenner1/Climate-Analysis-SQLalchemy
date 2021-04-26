09f2ed7a1c63b10f0d8dace0ab8b583508eefefd
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-04-25 20:59:44 -0500

    new

--- app.py
+++ app.py
@@ -5,14 +5,53 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import datetime as dt
 
+#set up database
+
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
+#new model for database
 Base = automap_base()
 
+#reflect the tables
 Base.prepare(engine, reflect=True)
 
+#references for tables 
 Measurement = Base.classes.measurement
 
 Station = Base.classes.station
 
+#create a session
 session = Session(engine)
+
+
+#set up flask 
+app = Flask(__name__)
+
+#flask route 
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+
+def calc_temps(start_date, end_date):
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    session = Session(engine)
+
+    return (
+        session.query(
+            func.min(Measurement.tobs),
+            func.avg(Measurement.tobs),
+            func.max(Measurement.tobs),
+        )
+        .filter(Measurement.date >= start_date)
+        .filter(Measurement.date <= end_date)
+        .all()
+    )
